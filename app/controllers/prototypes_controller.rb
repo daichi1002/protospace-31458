1ba627f6147187a773c3ee66ed22562238db5d9d
daichi1002 <72916511+daichi1002@users.noreply.github.com>
2020-11-03 11:57:51 +0900

    editアクションの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,5 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: :new
-  before_action :move_to_index, only: :edit
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -27,6 +26,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
@@ -48,11 +50,5 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
   end
 end
